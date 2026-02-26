2ef71493ee896957c576f53757381a634e0ad939
yanzhigang <yanzhigang>
2026-02-26 17:54:38 +0800

    可调式版本

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,104 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+class llConfigurationProvider implements vscode.DebugConfigurationProvider {
+
+	/**
+	 * Massage a debug configuration just before a debug session is being launched,
+	 * e.g. add all missing attributes to the debug configuration.
+	 */
+	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
+
+		// if launch.json is missing or empty
+		if (!config.type && !config.request && !config.name) {
+			const editor = vscode.window.activeTextEditor;
+			if (editor && editor.document.languageId === 'markdown') {
+				config.type = 'll';
+				config.name = 'Launch';
+				config.request = 'launch';
+				config.program = '${file}';
+				config.stopOnEntry = true;
+			}
+		}
+
+		if (!config.program) {
+			return vscode.window.showInformationMessage("Cannot find a program to debug").then(_ => {
+				return undefined;	// abort launch
+			});
+		}
+
+		return config;
+	}
+}
+class DebugAdapterExecutableFactory implements vscode.DebugAdapterDescriptorFactory {
+
+	// The following use of a DebugAdapter factory shows how to control what debug adapter executable is used.
+	// Since the code implements the default behavior, it is absolutely not neccessary and we show it here only for educational purpose.
+
+	createDebugAdapterDescriptor(_session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): ProviderResult<vscode.DebugAdapterDescriptor> {
+		// param "executable" contains the executable optionally specified in the package.json (if any)
+
+		// use the executable specified in the package.json if it exists or determine it based on some other information (e.g. the session)
+		if (!executable) {
+			const command = "absolute path to my DA executable";
+			const args = ["test"];
+			const options = {
+				cwd: "working directory for executable",
+				env: { "envVariable": "some value" }
+			};
+			executable = new vscode.DebugAdapterExecutable(command, args, options);
+		}
+		return new vscode.DebugAdapterExecutable(
+				executable.command, // dap.exe路径
+				['--port=4711'], // 传给dap.exe的参数（可选）
+				{ cwd: vscode.workspace.rootPath } // 工作目录
+		);
+		// make VS Code launch the DA executable
+		return executable;
+	}
+}
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ll" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('ll.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from ll!');
 		console.log(":123123");
 	});
-
 	context.subscriptions.push(disposable);
-}
 
-// This method is called when your extension is deactivated
+	context.subscriptions.push(vscode.commands.registerCommand('extension.ll-debug.getProgramName', config => {
+		return vscode.window.showInputBox({
+			placeHolder: "Please enter the name of a ll file in the workspace folder",
+			value: "trest.ll"
+		});
+	}));
+    // const provider = new llConfigurationProvider();
+	// context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('ll-debug', provider));
+    const factory = new DebugAdapterExecutableFactory();
+	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('ll-debug', factory));
+	// 可选：注册调试适配器追踪器，打印dap.exe的日志（方便调试）
+	context.subscriptions.push(
+		vscode.debug.registerDebugAdapterTrackerFactory('ll-debugger', {
+			createDebugAdapterTracker: (session) => {
+				return {
+				onDidSendMessage: (msg:any) => console.log('DAP Send:', msg),
+				onDidReceiveMessage: (msg:any) => console.log('DAP Receive:', msg)
+				};
+			}
+		})
+	);
+    // context.subscriptions.push(vscode.languages.registerEvaluatableExpressionProvider('ll-debug', {
+	// 	provideEvaluatableExpression(document: vscode.TextDocument, position: vscode.Position): vscode.ProviderResult<vscode.EvaluatableExpression> {
+	// 		console.log("表达式求值",position);
+	// 		return undefined;
+	// 	}
+	// }));
+	// context.subscriptions.push(vscode.languages.registerInlineValuesProvider('ll-debug', {
+	// 	provideInlineValues(document: vscode.TextDocument, viewport: vscode.Range, context: vscode.InlineValueContext) : vscode.ProviderResult<vscode.InlineValue[]> {
+	// 		const allValues: vscode.InlineValue[] = [];
+	// 		console.log("行内求值",);			
+	// 		return allValues;
+	// 	}
+	// }));
+
+}
 export function deactivate() {}
