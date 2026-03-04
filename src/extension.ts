b50c635ce16c84eda54d81e5ffc3220d3da1bf2a
yanzhigang <yanzhigang>
2026-03-04 14:25:13 +0800

    支持两种调试方式

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from 'vscode';
+import { spawn } from 'child_process';
 
 class llConfigurationProvider implements vscode.DebugConfigurationProvider {
 
@@ -30,31 +31,69 @@ class llConfigurationProvider implements vscode.DebugConfigurationProvider {
 		return config;
 	}
 }
+
+function spawnSyncWrapper(command:string, args?: readonly string[],options?: object) : any{
+  return new Promise((resolve, reject) => {
+    const child = spawn(command, args,options);
+	child.stdout.setEncoding('utf8');
+    // 拼接标准输出
+    child.stdout.on('data', (data) => {
+	  resolve({ code:200,"data":data });
+    });
+
+    // 拼接标准错误
+    child.stderr.on('data', (data) => {
+	  reject({ code:500,"data":`子进程输出错误:${data}` });
+    });
+
+    // 进程退出时返回结果
+    child.on('close', (code) => {
+	  reject({ code:500,"data":`子进程输出错误:子进程退出启动` });
+    });
+
+    // 进程启动失败（如命令不存在）
+    child.on('error', (err) => {
+	  reject({ code:500,"data":`子进程启动失败：${err.message}`});
+    });
+  });
+}
 class DebugAdapterExecutableFactory implements vscode.DebugAdapterDescriptorFactory {
 
 	// The following use of a DebugAdapter factory shows how to control what debug adapter executable is used.
 	// Since the code implements the default behavior, it is absolutely not neccessary and we show it here only for educational purpose.
 
-	createDebugAdapterDescriptor(_session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): ProviderResult<vscode.DebugAdapterDescriptor> {
-		// param "executable" contains the executable optionally specified in the package.json (if any)
-
-		// use the executable specified in the package.json if it exists or determine it based on some other information (e.g. the session)
-		if (!executable) {
-			const command = "absolute path to my DA executable";
-			const args = ["test"];
-			const options = {
-				cwd: "working directory for executable",
-				env: { "envVariable": "some value" }
-			};
-			executable = new vscode.DebugAdapterExecutable(command, args, options);
+	async createDebugAdapterDescriptor(_session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable): Promise<ProviderResult<vscode.DebugAdapterDescriptor>> {
+		const port = _session.configuration.port ;
+		let command = "";
+		if(!executable)
+		{
+			vscode.window.showErrorMessage(`启动调试适配器失败: 无法找到ll_debug的调试适配器`);
+			throw new Error('启动调试适配器失败: 无法找到ll_debug的调试适配器');
 		}
-		return new vscode.DebugAdapterExecutable(
-				executable.command, // dap.exe路径
-				['--port=4711'], // 传给dap.exe的参数（可选）
-				{ cwd: vscode.workspace.rootPath } // 工作目录
-		);
-		// make VS Code launch the DA executable
-		return executable;
+		else
+		{
+			command = executable.command;
+			if(port > -1)
+			{
+				let newPort = port;
+				const result = await spawnSyncWrapper(command, [`--port=${port}`],{cwd: vscode.workspace.rootPath,stdio: ['pipe', 'pipe', 'pipe']});
+				console.log('ll DebugStart：', result);
+				if(result.code == 200)
+				{
+					newPort = Number.parseInt(result.data);		
+					return new vscode.DebugAdapterServer(newPort);					
+				}
+				else
+				{
+					vscode.window.showErrorMessage(`启动调试适配器失败: 无法找到ll_debug的调试适配器`);
+					throw new Error('启动调试适配器失败: 无法找到ll_debug的调试适配器');
+				}
+			}
+			else
+			{
+				return new vscode.DebugAdapterExecutable(command,[''], { cwd: vscode.workspace.rootPath });
+			}
+		}
 	}
 }
 export function activate(context: vscode.ExtensionContext) {
@@ -64,13 +103,6 @@ export function activate(context: vscode.ExtensionContext) {
 		console.log(":123123");
 	});
 	context.subscriptions.push(disposable);
-
-	context.subscriptions.push(vscode.commands.registerCommand('extension.ll-debug.getProgramName', config => {
-		return vscode.window.showInputBox({
-			placeHolder: "Please enter the name of a ll file in the workspace folder",
-			value: "trest.ll"
-		});
-	}));
     // const provider = new llConfigurationProvider();
 	// context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('ll-debug', provider));
     const factory = new DebugAdapterExecutableFactory();
